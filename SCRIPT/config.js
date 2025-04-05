70dd18ab691fa18384551351abc48b0e4c96e244
sidstedt <daniel.sidstedt@gmail.com>
2025-04-05 22:52:47 +0200

    renamed folder for js files

---
+++ SCRIPT/config.js
@@ -0,0 +1,7 @@
+const API_KEYS = {
+  geocode: "67e29d0c7070e762805581tqfb1b7fd",
+  weather: "b3038e407c394a9b921131618252503",
+  unsplash: "YMcPNtDlkdmAXxU25FpC6fyrRwXoeeKcVIHvWprZ1eQ",
+};
+
+export default API_KEYS;
